1a389ff3e456a7addcdd952cd2c6810cbc59cedf
telco2011 <davidlopez.david@gmail.com>
2018-02-02 15:07:29 +1000

    updated specs

--- 1.1-simple-rest-service/src/controllers/api/helloworld/helloworld.dto.spec.ts
+++ 1.1-simple-rest-service/src/controllers/api/helloworld/helloworld.dto.spec.ts
@@ -1,80 +1,18 @@
 import { suite, test, YggdrasilTest } from '@yggdrasil/testing';
 
-import { app } from '../ignition';
+import { HelloWorld } from './helloworld.dto';
 
 process.env.NODE_ENV = 'test';
 
-@suite('Basic tests')
-class BasicTestSuite extends YggdrasilTest {
+@suite('api.helloworld.dto tests')
+class HelloWorldDTOTestSuite extends YggdrasilTest {
 
-  constructor() {
-    super(app);
+  @test('should buid new HelloWorld DTO.')
+  public testDefaultConstructor(done) {
+    const hw = new HelloWorld(0, 'World');
+
+    this.chai.assert.deepEqual(hw, { id: 0, name: 'World' });
+    done();
   }
 
-  @test('should test the default yggdrasil access.')
-  public testAccess(done) {
-    this.chai.request(this.server)
-      .get('/')
-      .end((err, res) => {
-        this.should.not.exist(err);
-        res.should.have.status(200);
-        done();
-      });
-  }
-
-  @test('should test get hello world response')
-  public testGetHelloWorld(done) {
-    this.chai.request(this.server)
-      .get('/api/basic')
-      .end((err, res) => {
-        this.should.not.exist(err);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.a.property('method');
-        res.body.method.should.be.equal('GET');
-        done();
-      });
-  }
-
-  @test('should test post hello world response')
-  public testPostHelloWorld(done) {
-    this.chai.request(this.server)
-      .post('/api/basic')
-      .end((err, res) => {
-        this.should.not.exist(err);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.a.property('method');
-        res.body.method.should.be.equal('POST');
-        done();
-      });
-  }
-
-  @test('should test put hello world response')
-  public testPutHelloWorld(done) {
-    this.chai.request(this.server)
-      .put('/api/basic')
-      .end((err, res) => {
-        this.should.not.exist(err);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.a.property('method');
-        res.body.method.should.be.equal('PUT');
-        done();
-      });
-  }
-
-  @test('should test delete hello world response')
-  public testDeleteHelloWorld(done) {
-    this.chai.request(this.server)
-      .delete('/api/basic')
-      .end((err, res) => {
-        this.should.not.exist(err);
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.a.property('method');
-        res.body.method.should.be.equal('DELETE');
-        done();
-      });
-  }
 }
